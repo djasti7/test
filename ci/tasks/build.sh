326374a51d1c89697282759d49f6dfbc86ad6921
SI + DJ <si+dj@fiserv.com>
2019-06-18 10:30:55 -0400

    Modify test and build script

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -4,14 +4,17 @@ cd service-repo
 echo I am in `pwd`
 ls -l ../
 
-echo "starting build ..."
 cd TDDDemoAppTests
+echo I am in `pwd`
+ls -l ../
+
+echo "starting test ..."
 dotnet test
 
 cd ../TDDDemoApp
-dotnet restore
-dotnet publish -c Release -o ./publish
+echo I am in `pwd`
+ls -l ../
 
-echo "copying files to ../build-output"
-cp manifest.yml ../build-output
-cp -R ./publish/* ../build-output
+echo "starting restore ..."
+dotnet restore
+dotnet build
