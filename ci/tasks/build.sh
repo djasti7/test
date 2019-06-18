39d6bf54020754b36e37930c54dc269f39e1e2b7
SI + DJ <si+dj@fiserv.com>
2019-06-18 10:44:51 -0400

    Change manifest.yml path on build

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,19 +2,24 @@
 cd service-repo
 
 echo I am in `pwd`
-ls -l ../
+ls -l
 
 cd TDDDemoAppTests
 echo I am in `pwd`
-ls -l ../
+ls -l
 
 echo "starting test ..."
 dotnet test
 
 cd ../TDDDemoApp
 echo I am in `pwd`
-ls -l ../
+ls -l
 
 echo "starting restore ..."
 dotnet restore
 dotnet build
+dotnet publish -c Release -o ./publish
+
+echo "copying files to ../../build-output"
+cp ../manifest.yml ../../build-output
+cp -R ./publish/* ../../build-output
